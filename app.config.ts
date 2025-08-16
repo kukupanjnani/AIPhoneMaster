7ca11a79fe51388cb53ae5a55736af97009aae89
Shiv <shiv.kkhospitality@gmail.com>
2025-08-16 04:00:15 +0000

    chore(eas): set projectId and add eas.json

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ const config: ExpoConfig = {
   orientation: 'portrait',
   platforms: ['ios', 'android'],
   extra: {
-    eas: { projectId: 'YOUR_EAS_PROJECT_ID' }
+  eas: { projectId: '3e3bbdd4-d1be-4d0f-90a4-1020a49b0e73' }
   }
 };
 
